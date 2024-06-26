7dc71dfeae619bd6eaf4a5902d371b9c03c2b72f
Ivan <ikiduk2010@gmail.com>
2024-06-26 10:28:00 +0300

    V 1.0.0

--- src/main/java/com/navi92/cutscenemod/item/custom/VideoViewerItem.java
+++ src/main/java/com/navi92/cutscenemod/item/custom/VideoViewerItem.java
@@ -1,7 +1,7 @@
 package com.navi92.cutscenemod.item.custom;
 
 import com.navi92.cutscenemod.client.ClientHooks;
-import com.navi92.cutscenemod.utils.FileImporter;
+//import com.navi92.cutscenemod.utils.FileImporter;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -21,15 +21,15 @@ public class VideoViewerItem extends Item {
         if (pContext.getLevel().isClientSide) {
             if (pContext.getHand().equals(InteractionHand.MAIN_HAND)) {
                 pContext.getPlayer().sendSystemMessage(Component.literal("Item 'Cutscene Viewer' was used!"));
-//                DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () ->
-//                        ClientHooks.openCutscenePlayerScreen(
-//                                pContext.getPlayer(),
-//                                "rick",
-//                                854,
-//                                480,
-//                                200));
-                FileImporter.importFrames("C:\\Users\\ikidu\\Downloads\\Telegram Desktop\\rick.mp4",
-                        "main\\resources\\assets\\cutscenemod\\textures\\cutscenes");
+                DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () ->
+                        ClientHooks.openCutscenePlayerScreen(
+                                pContext.getPlayer(),
+                                "rick",
+                                854,
+                                480,
+                                200));
+//                FileImporter.importFrames("C:\\Users\\ikidu\\Downloads\\Telegram Desktop\\rick.mp4",
+//                        "main\\resources\\assets\\cutscenemod\\textures\\cutscenes");
                 return InteractionResult.SUCCESS;
             }
         }
